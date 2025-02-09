3bdec7c7dd659eb16c274c6b7ec1b95910044621
Chad Transtrum <chad@transtrum.net>
2025-02-09 08:33:55 -0700

    add the SHOW_LAYER_COLOR variable

--- include/zmk_rgbled_widget/widget.h
+++ include/zmk_rgbled_widget/widget.h
@@ -2,6 +2,10 @@
     (IS_ENABLED(CONFIG_RGBLED_WIDGET_SHOW_LAYER_CHANGE)) &&                                        \
         (!IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL))
 
+#define SHOW_LAYER_COLORS                                                                          \
+    (IS_ENABLED(CONFIG_RGBLED_WIDGET_SHOW_LAYER_COLORS)) &&                                        \
+        (!IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL))
+
 #if IS_ENABLED(CONFIG_ZMK_BATTERY_REPORTING)
 void indicate_battery(void);
 #endif
